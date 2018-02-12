b2f30e589eb77ca53e94ebbc16a1d56e06425b08
maiux <andreamaioli92@gmail.com>
2018-02-12 12:57:37 +0100

    Init

---
+++ test.ino
@@ -0,0 +1,8 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+	DigiKeyboard.print("abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789 \\|!\"$%&/()=?^<>[]{}@#,;.:-_");
+
+}
+
+void loop() {}
